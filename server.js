47a9fc23fed8faf4956fa729935bb53a5df0e0f4
Mitchell Wood <mitchellwood11@gmail.com>
2023-01-18 10:03:31 -0600

    initial inquirer prompt/cases

--- server.js
+++ server.js
@@ -4,19 +4,73 @@ const inquirer = require("inquirer");
 require("console.table");
 
 //db connection
-const connection = mysql.createConnection({
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: '603Bondstreet!',
-        database: 'employees_db'
-    },
-    console.log('database connection successful!')
-)
-
+const connection = mysql.createConnection(
+  {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "603Bondstreet!",
+    database: "employees_db",
+  },
+  console.log("database connection successful!")
+);
 
 connection.connect(function (err) {
-    if (err) throw err;
-    console.log(`Employee Manager Online!`)
-    console.log("connected as id " + connection.threadId);
+  if (err) throw err;
+  console.log(`Employee Manager Online!`);
+  console.log("connected as id " + connection.threadId);
+  initialPrompt();
 });
+
+function initialPrompt() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "task",
+      message: "Would you like to do?",
+      choices: [
+        "View Employees",
+        "View Employees by Department",
+        "Add Employee",
+        "Remove Employees",
+        "Update Employee Role",
+        "Add Role",
+        "End",
+      ],
+    })
+    .then(function ({ task }) {
+      switch (task) {
+        case "View Employees":
+          viewEmployee();
+          break;
+
+        case "View Employees by Department":
+          viewEmployeeByDepartment();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Remove Employees":
+          removeEmployees();
+          break;
+
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "End":
+          connection.end();
+          break;
+      }
+    });
+}
+
+function viewEmployee() {
+    console.log('This is working!')
+}
